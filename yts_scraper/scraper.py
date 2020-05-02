11192d4dcb688e7cfe104cd81b2a49c8df5810e2
Özenç Bilgili <bilgiliozenc@gmail.com>
2020-05-02 03:46:34 +0300

    Linting and reformating

--- yts_scraper/scraper.py
+++ yts_scraper/scraper.py
@@ -1,100 +1,117 @@
-import json
-import requests
 import os
 import sys
 import math
-import string
-import time
+import json
 from concurrent.futures.thread import ThreadPoolExecutor
+import requests
 from tqdm import tqdm
 from fake_useragent import UserAgent
 
 class Scraper:
+    """
+    Scraper class.
+
+    Must be initialized with args from argparser
+    """
     # Constructor
     def __init__(self, args):
-        self.args = args
-        return
-
-    # Argument validation
-    def __validate_args(self):
-        args = self.args
-
-        # Set output directory
-        if args.output:
-            os.makedirs(args.output, exist_ok=True)
-            directory = os.path.curdir + '/' + args.output
-        else:
-            os.makedirs(args.categorize_by.title(), exist_ok=True)
-            directory = os.path.curdir + '/' + args.categorize_by.title()
-
-        # Args for downloading in reverse chronological order     
-        if args.sort_by == 'latest':
-            self.sort_by = 'date_added'
-            self.order_by = 'desc'
-        else:
-            self.sort_by = args.sort_by
-            self.order_by = "asc"
-            
-        self.directory = directory
-        self.quality = '3D' if (args.quality == '3d') else args.quality # Check 3D arg casing 
+        self.output = args.output
         self.genre = args.genre
         self.minimum_rating = args.rating
+        self.quality = '3D' if (args.quality == '3d') else args.quality
         self.categorize = args.categorize_by
+        self.sort_by = args.sort_by
         self.year_limit = args.year_limit
         self.page_arg = args.page
         self.poster = args.background
         self.imdb_id = args.imdb_id
         self.multiprocess = args.multiprocess
 
-    # Connect to API and extract initial data
-    def __get_api_data(self):
+        self.movie_count = None
+        self.url = None
+        self.existing_file_counter = None
+        self.skip_exit_condition = None
+        self.downloaded_movie_ids = None
+        self.pbar = None
+
+
+        # Set output directory
+        if self.output:
+            os.makedirs(self.output, exist_ok=True)
+            self.directory = os.path.join(os.path.curdir, self.output)
+        else:
+            os.makedirs(self.categorize.title(), exist_ok=True)
+            self.directory = os.path.join(os.path.curdir, self.categorize.title())
+
+
+        # Args for downloading in reverse chronological order
+        if self.sort_by == 'latest':
+            self.sort_by = 'date_added'
+            self.order_by = 'desc'
+        else:
+            self.order_by = 'asc'
+
+
         # YTS API has a limit of 50 entries
         self.limit = 50
-        
+
+
+    # Connect to API and extract initial data
+    def __get_api_data(self):
         # Formatted URL string
-        url = 'https://yts.mx/api/v2/list_movies.json?quality={quality}&genre={genre}&minimum_rating={minimum_rating}&sort_by={sort_by}&order_by={order_by}&limit={limit}&page='.format(
-            quality = self.quality, 
-            genre = self.genre, 
-            minimum_rating = self.minimum_rating, 
-            sort_by = self.sort_by, 
-            order_by = self.order_by, 
-            limit = self.limit
-        )
+        url = '''https://yts.mx/api/v2/list_movies.json?
+                 quality={quality}&
+                 genre={genre}&
+                 minimum_rating={minimum_rating}&
+                 sort_by={sort_by}&
+                 order_by={order_by}&
+                 limit={limit}&
+                 page='''.format(
+                     quality=self.quality,
+                     genre=self.genre,
+                     minimum_rating=self.minimum_rating,
+                     sort_by=self.sort_by,
+                     order_by=self.order_by,
+                     limit=self.limit
+                 )
 
         # Generate random user agent header
         try:
-            ua = UserAgent()
-            headers = {'User-Agent': ua.random}
+            user_agent = UserAgent()
+            headers = {'User-Agent': user_agent.random}
         except:
-            print("Error occurred during fake user agent generation.")
+            print('Error occurred during fake user agent generation.')
 
         # Exception handling for connection errors
         try:
-            r = requests.get(url,timeout=5, verify=True, headers=headers)
-            r.raise_for_status()
+            req = requests.get(url, timeout=5, verify=True, headers=headers)
+            req.raise_for_status()
         except requests.exceptions.HTTPError as errh:
-            print("HTTP Error:",errh)
-            exit(0)
+            print('HTTP Error:', errh)
+            sys.exit(0)
         except requests.exceptions.ConnectionError as errc:
-            print("Error Connecting:",errc)
-            exit(0)
+            print('Error Connecting:', errc)
+            sys.exit(0)
         except requests.exceptions.Timeout as errt:
-            print("Timeout Error:",errt)
-            exit(0)
+            print('Timeout Error:', errt)
+            sys.exit(0)
         except requests.exceptions.RequestException as err:
-            print("There was an error.",err)
-            exit(0)
+            print('There was an error.', err)
+            sys.exit(0)
 
         # Exception handling for JSON decoding errors
         try:
-            data = r.json()
+            data = req.json()
         except json.decoder.JSONDecodeError:
-            print("Could not decode JSON")
+            print('Could not decode JSON')
 
-        
+
         # Adjust movie count according to starting page
-        movie_count = data['data']['movie_count'] if (self.page_arg == 1) else ((data['data']['movie_count']) - ((self.page_arg - 1) * self.limit))
-        
+        if self.page_arg == 1:
+            movie_count = data['data']['movie_count']
+        else:
+            movie_count = (data['data']['movie_count']) - ((self.page_arg - 1) * self.limit)
+
         self.movie_count = movie_count
         self.url = url
 
@@ -103,75 +120,93 @@ class Scraper:
         self.existing_file_counter = 0
         self.skip_exit_condition = False
 
-        # YTS API sometimes returns duplicate objects and 
+        # YTS API sometimes returns duplicate objects and
         # the script tries to download the movie more than once.
-        # IDs of downloaded movie is stored in this array 
+        # IDs of downloaded movie is stored in this array
         # to check if it's been downloaded before
         self.downloaded_movie_ids = []
 
-        # Calculate page count and make sure that it doesn't get the value of 1 to prevent range(1, 1)
-        page_count = 2 if ((math.trunc(self.movie_count / self.limit) + 1) == 1) else (math.trunc(self.movie_count / self.limit) + 1)
+        # Calculate page count and make sure that it doesn't
+        # get the value of 1 to prevent range(1, 1)
+        if math.trunc(self.movie_count / self.limit) + 1 == 1:
+            page_count = 2
+        else:
+            page_count = math.trunc(self.movie_count / self.limit) + 1
 
         range_ = range(int(self.page_arg), page_count)
 
 
-        print("Initializing download with these parameters:")
-        print("\t\nDirectory:\t%s\t\nQuality:\t%s\t\nMovie Genre:\t%s\t\nMinimum Rating:\t%s\t\nCategorization:\t%s\t\nMinimum Year:\t%s\t\nStarting page:\t%s\nMovie posters:\t%s\nAppend IMDb ID:\t%s\nMultiprocess:\t%s\n" % 
-            (self.directory,
-            self.quality,
-            self.genre,
-            self.minimum_rating,
-            self.categorize,
-            self.year_limit,
-            self.page_arg,
-            str(self.poster),
-            str(self.imdb_id),
-            str(self.multiprocess))
+        print('Initializing download with these parameters:\n')
+        print('Directory:\t{}\nQuality:\t{}\nMovie Genre:\t{}\nMinimum Rating:\t{}\nCategorization:\t{}\nMinimum Year:\t{}\nStarting page:\t{}\nMovie posters:\t{}\nAppend IMDb ID:\t{}\nMultiprocess:\t{}\n'
+              .format(
+                  self.directory,
+                  self.quality,
+                  self.genre,
+                  self.minimum_rating,
+                  self.categorize,
+                  self.year_limit,
+                  self.page_arg,
+                  str(self.poster),
+                  str(self.imdb_id),
+                  str(self.multiprocess)
+                  )
+             )
+
+        if self.movie_count <= 0:
+            print('Could not find any movies with given parameters')
+            sys.exit(0)
+        else:
+            print('Query was successful.')
+            print('Found {} movies. Download starting...\n'.format(self.movie_count))
+
+        # Create progress bar
+        self.pbar = tqdm(
+            total=self.movie_count,
+            position=0,
+            leave=True,
+            desc='Downloading',
+            unit='Files'
             )
 
-        if (self.movie_count <= 0):
-            print("Could not find any movies with given parameters")
-            exit(0)
-        else:
-            print("Query was successful.\nFound %d movies. Download starting...\n" % (self.movie_count))
-        
-        # Create progress bar
-        self.pbar = tqdm(total=self.movie_count, position=0, leave=True, desc='Downloading', unit='Files')
-        
         # Multiprocess executor
         # Setting max_workers to None makes executor utilize CPU number * 5 at most
         executor = ThreadPoolExecutor(max_workers=None)
-        
+
         for page in range_:
-            url = self.url + str(page)
+            url = '{}{}'.format(self.url, str(page))
 
             # Generate random user agent header
             try:
-                ua = UserAgent()
-                headers = {'User-Agent': ua.random}
+                user_agent = UserAgent()
+                headers = {'User-Agent': user_agent.random}
             except:
-                print("Error occurred during fake user agent generation.")
+                print('Error occurred during fake user agent generation.')
 
             # Send request to API
-            page_response = requests.get(url,timeout=5, verify=True, headers=headers).json()
-            
+            page_response = requests.get(url, timeout=5, verify=True, headers=headers).json()
+
             movies = page_response['data']['movies']
-            
+
             # Movies found on current page
             if not movies:
-                print("Could not find any movies on this page.\n")
+                print('Could not find any movies on this page.\n')
 
             if self.multiprocess:
                 # Wrap tqdm around executor to update pbar with every process
-                tqdm(executor.map(self.__filter_torrents, movies), total=self.movie_count, position=0, leave=True)       
-            
+                tqdm(
+                    executor.map(self.__filter_torrents, movies),
+                    total=self.movie_count,
+                    position=0,
+                    leave=True
+                    )
+
             else:
                 for movie in movies:
                     self.__filter_torrents(movie)
 
         self.pbar.close()
-        print("Download finished.")
-        
+        print('Download finished.')
+
 
     # Determine which .torrent files to download
     def __filter_torrents(self, movie):
@@ -187,7 +222,7 @@ class Scraper:
         # Every torrent option for current movie
         torrents = movie['torrents']
         # Remove illegal file/directory characters
-        movie_name = movie['title_long'].translate({ord(i):None for i in '/\:*?"<>|'})
+        movie_name = movie['title_long'].translate({ord(i):None for i in "'/\:*?<>|"})
 
         # Used to multiple download messages for multi-folder categorization
         is_download_successful = False
@@ -196,19 +231,19 @@ class Scraper:
             return
 
         # In case movie has no available torrents
-        if torrents == None:
-            tqdm.write("Could not find any torrents for " + movie_name + ". Skipping...")
+        if torrents is None:
+            tqdm.write('Could not find any torrents for {}. Skipping...'.format(movie_name))
             return
 
         bin_content_img = (requests.get(movie['large_cover_image'])).content if self.poster else None
-        
+
         # Iterate through available torrent files
         for torrent in torrents:
             quality = torrent['quality']
             if self.categorize and self.categorize != 'rating':
                 if self.quality == 'all' or self.quality == quality:
                     bin_content_tor = (requests.get(torrent['url'])).content
-                    
+
                     for genre in movie_genres:
                         path = self.__build_path(movie_name, movie_rating, quality, genre, imdb_id)
                         is_download_successful = self.__download_file(bin_content_tor, bin_content_img, path, movie_name, movie_id)
@@ -217,16 +252,16 @@ class Scraper:
                     bin_content_tor = (requests.get(torrent['url'])).content
                     path = self.__build_path(movie_name, movie_rating, quality, None, imdb_id)
                     is_download_successful = self.__download_file(bin_content_tor, bin_content_img, path, movie_name, movie_id)
-            
+
             if is_download_successful and self.quality == 'all' or self.quality == quality:
-                tqdm.write("Downloaded " + movie_name + " " + quality.upper())
+                tqdm.write('Downloaded {} {}'.format(movie_name, quality.upper()))
                 self.pbar.update()
 
 
-    # Creates a file path for each download 
+    # Creates a file path for each download
     def __build_path(self, movie_name, rating, quality, movie_genre, imdb_id):
         directory = self.directory
-        
+
         if self.categorize == 'rating':
             directory += '/' + str(math.trunc(rating)) + '+'
         elif self.categorize == 'genre':
@@ -241,8 +276,11 @@ class Scraper:
 
         os.makedirs(directory, exist_ok=True)
 
-        filename = (movie_name + ' ' + quality + ' - ' + imdb_id) if self.imdb_id else (movie_name + ' ' + quality)
-        
+        if self.imdb_id:
+            filename = '{} {} - {}'.format(movie_name, quality, imdb_id)
+        else:
+            filename = '{} {}'.format(movie_name, quality)
+
         path = os.path.join(directory, filename)
         return path
 
@@ -252,36 +290,35 @@ class Scraper:
             self.__prompt_existing_files()
 
         if os.path.isfile(path):
-            tqdm.write(movie_name + ": File already exists. Skipping...")
+            tqdm.write('{}: File already exists. Skipping...'.format(movie_name))
             self.existing_file_counter += 1
             return False
-        else:
-            with open((path + '.torrent'), 'wb') as t:
-                t.write(bin_content_tor)
-            if self.poster:
-                with open((path + '.jpg'), 'wb') as t:
-                    t.write(bin_content_img)
-                
-            self.downloaded_movie_ids.append(movie_id)
-            self.existing_file_counter = 0 
-            return True
-    
+
+        with open(path + '.torrent', 'wb') as torrent:
+            torrent.write(bin_content_tor)
+        if self.poster:
+            with open(path + '.jpg', 'wb') as torrent:
+                torrent.write(bin_content_img)
+
+        self.downloaded_movie_ids.append(movie_id)
+        self.existing_file_counter = 0
+        return True
+
     # Is triggered when the script hits 10 consecutive existing files
     def __prompt_existing_files(self):
-        tqdm.write("Found 10 existing files in a row. Do you want to keep downloading? Y/N")
+        tqdm.write('Found 10 existing files in a row. Do you want to keep downloading? Y/N')
         exit_answer = input()
 
         if exit_answer.lower() == 'n':
-            tqdm.write("Exiting...")
-            exit()
+            tqdm.write('Exiting...')
+            sys.exit(0)
         elif exit_answer.lower() == 'y':
-            tqdm.write("Continuing...")
+            tqdm.write('Continuing...')
             self.existing_file_counter = 0
             self.skip_exit_condition = True
         else:
             tqdm.write('Invalid input. Enter "Y" or "N".')
 
     def download(self):
-        self.__validate_args()
         self.__get_api_data()
         self.__initialize_download()
